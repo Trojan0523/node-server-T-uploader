2b3319d6ceacf7fd65e2005a874341da13a52c3b
trojan0523 <1153532327@qq.com>
2021-02-19 00:34:04 +0800

    fix: unkonwn bug

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const multer = require('multer');
-const upload = multer({dest: 'uploads/'})
-const cors = require('cors')
+const cors = require('cors');
+const upload = multer({ dest: 'uploads/' });
+
 const app = express();
-const path = require('path');
+
 app.get('/', (req,res) => {
     res.send('hello nodejs')
 })
-app.post('/upload', cors(), upload.single('file'), (req,res) => {
-    // single后面的类型是协商好的
-    res.send(req.file.filename)
-})
 app.options('/upload', cors())
-app.get('/preview:key', cors(), (req,res) => {
+
+app.post('/upload', cors(), upload.single('file'), function (req, res) {
+    console.log(req.body)
+    res.send(req.file.filename);
+});
+
+app.get('/preview/:key', cors(), (req,res) => {
     console.log(req.params.key)
     res.sendFile(`uploads/${req.params.key}`, { 
         root: __dirname,
@@ -21,7 +24,8 @@ app.get('/preview:key', cors(), (req,res) => {
         },
     }, (err) => {
         if(err) {
-            res.status(404).send('Not Found')
+            console.log(err);
+            res.status(404).send('Not Found');
         }
     });
 });
